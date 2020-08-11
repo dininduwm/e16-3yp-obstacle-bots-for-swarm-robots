e3f42e7d741fb541e13967bb53244ba34d88aa1a
thusharakart <thusharakart@gmail.com>
2020-08-11 12:41:13 +0530

    positioning_algo edited

--- Main_Code/positioning_algo.py
+++ Main_Code/positioning_algo.py
@@ -1,18 +1,23 @@
 from math import atan, sqrt
 
-def positions(start_x, start_y, start_gradient, end_x, end_y, end_gradient):
+def positions(start_pos, start_angle, end_pos, end_angle):
     # calculate the required gradient
-    required_gradient = (end_y - start_y) / (end_x - start_y)
-    # calculate the start angle 
-    start_angle = atan(start_y / start_x)
-    # calculate the end angle of the robot
-    end_angle = atan(end_y / end_x)
+    required_gradient = (end_pos[1] - start_pos[1]) / (end_pos[0] - start_pos[0])
     # calculate howmany degrees does the robot turn at the beginning
     start_turn = atan(required_gradient) - start_angle
     # calculate howfar does the robot should move
-    distance = sqrt(pow((end_y - start_y), 2) + pow((end_x - start_y), 2))
+    distance = sqrt(pow((end_pos[1] - start_pos[1]), 2) + pow((end_pos[0] - start_pos[0]), 2))
     # calculate howmany degrees does the robot turn at the end
-    end_turn = end_angle - start_turn
+    end_turn = end_angle - atan(required_gradient)
 
-    return (start_turn, distance, end_turn)
+    # neglect the small angles
+    min_angle = 0.03491
+    min_distance = 10
+    
+    start_turn = start_turn if abs(start_turn) > min_angle else 0
+    end_turn = end_turn if abs(end_turn) > min_angle else 0
+    distance = distance if abs(distance) > min_distance else 0
+
+    return (round(start_turn, 4), round(distance, 4), round(end_turn, 4))
+
 
