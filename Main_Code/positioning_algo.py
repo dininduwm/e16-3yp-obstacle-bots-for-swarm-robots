6d661a454eccdb54b7197e1f8d633b67e9fe3a74
thusharakart <thusharakart@gmail.com>
2020-08-13 17:33:12 +0530

    positioning_algo edited

--- Main_Code/positioning_algo.py
+++ Main_Code/positioning_algo.py
@@ -10,7 +10,7 @@ def positions(start_pos, start_angle, end_pos, end_angle):
     # calculate the required angle
     required_angle = atan(required_gradient)#3.14159 - atan(required_gradient) if (pow(start_pos[0], 2) + pow(start_pos[1], 2) 
         #     < pow(end_pos[0], 2) + pow(end_pos[1], 2)) else atan(required_gradient)
-    
+        
     # calculate howmany degrees does the robot turn at the beginning
     start_turn = required_angle - start_angle
     # calculate howfar does the robot should move
@@ -21,10 +21,14 @@ def positions(start_pos, start_angle, end_pos, end_angle):
     # neglect the small angles
     min_angle = 0.03491
     min_distance = 10
+
+    start_angle = start_angle if end_pos[1] < start_pos[1] else 180 - start_angle
     
     start_turn = start_turn if abs(start_turn) > min_angle else 0
     end_turn = end_turn if abs(end_turn) > min_angle else 0
     distance = distance if abs(distance) > min_distance else 0
+
+
 
     return (round(start_turn, 4), round(distance, 4), round(end_turn, 4))
 
