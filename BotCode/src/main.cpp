4be6d4964480eac8ab0222128cef7f57090a6a6d
heshads <tharindudissa18@gmail.com>
2020-08-13 09:33:23 +0530

    gyro , motor, tuning PID

--- BotCode/src/main.cpp
+++ BotCode/src/main.cpp
@@ -1,6 +1,30 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h> // including the software serial library
 #include <ArduinoJson.h>    // json phrasing library
+#include <PID_v1.h>
+#include <Wire.h>
+
+#define EN_R 9
+#define EN_L 10
+
+#define ML_A1 5
+#define ML_A2 4
+
+#define MR_A1 3
+#define MR_A2 2
+
+void calculate_IMU_error();
+void updateGyro();
+void MR(int val);
+void ML(int val);
+double radToDegree(double rads);
+
+const int MPU = 0x68; // MPU6050 I2C address
+float GyroX, GyroY, GyroZ;
+float angle;
+float GyroErrorX;
+float elapsedTime, currentTime, previousTime;
+int c = 0;
 
 // id of the bot
 String myID = "1";
@@ -12,10 +36,54 @@ SoftwareSerial mySerial(6, 12);
 double startAngle;
 double endAngle;
 double travelDis;
+double Setpoint, Input, Output;
+double mov_Setpoint, mov_Input, mov_Output;
 
 // variables to hold temp data
 String reciveStr = "";
 StaticJsonDocument<250> doc; // json Doc
+PID myPID(&Input, &Output, &Setpoint, 7, 0, 0.35, DIRECT);
+PID moving(&Input, &Output, &Setpoint, 2, 0, 0.3, DIRECT);
+
+// function to calculate the gyro error
+void calculate_IMU_error()
+{
+  // init the gyro0
+  Wire.begin();                // Initialize comunication
+  Wire.beginTransmission(MPU); // Start communication with MPU6050 // MPU=0x68
+  Wire.write(0x6B);            // Talk to 0 register 6B
+  Wire.write(0x00);            // Make reset - place a 0 into the 6B register
+  Wire.endTransmission(true);  //end the transmission
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1B); // Talk to the GYRO_CONFIG register (1B hex)
+  Wire.write(0x10); // Set the register bits as 00010000 (1000deg/s full scale)
+  Wire.endTransmission(true);
+  delay(20);
+
+  // Read gyro values 200 times
+  while (c < 200)
+  {
+    Wire.beginTransmission(MPU);
+    Wire.write(0x43);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 6, true);
+    GyroX = Wire.read() << 8 | Wire.read();
+    GyroY = Wire.read() << 8 | Wire.read();
+    GyroZ = Wire.read() << 8 | Wire.read();
+    // Sum all readings
+    GyroErrorX = GyroErrorX + (GyroX / 32.75);
+    c++;
+  }
+  //Divide the sum by 200 to get the error value
+  GyroErrorX = GyroErrorX / 200;
+
+  // Print the error values on the Serial Monitor
+  Serial.print("GyroErrorX: ");
+  Serial.println(GyroErrorX);
+}
+
+int count = 0;//temp
 
 // function to decode
 void parseJson(char c)
@@ -33,7 +101,11 @@ void parseJson(char c)
     DeserializationError error = deserializeJson(doc, char_array);
 
     // writing data to variables
-    startAngle = doc[myID][0];
+    if(count == 3){
+      startAngle = doc[myID][0];
+      count = 0;
+    }
+    count++;
     endAngle = doc[myID][2];
     travelDis = doc[myID][1];
 
@@ -63,25 +135,119 @@ void parseJson(char c)
 
 void setup()
 {
+  pinMode(EN_R, OUTPUT);
+  pinMode(EN_L, OUTPUT);
+  pinMode(MR_A1, OUTPUT);
+  pinMode(MR_A2, OUTPUT);
+  pinMode(ML_A1, OUTPUT);
+  pinMode(ML_A2, OUTPUT);
+
   // begining the serial commiunication
   mySerial.begin(9600); // Setting the baud rate of HC-12 Module
   Serial.begin(9600);   // Setting the baud rate of Serial Monitor (Arduino)
+
+  myPID.SetOutputLimits(-255, 255);
+  myPID.SetSampleTime(20);
+  myPID.SetMode(AUTOMATIC);
+  Setpoint = 0;
+
+  moving.SetOutputLimits(-255, 255);
+  moving.SetSampleTime(20);
+  moving.SetMode(AUTOMATIC);
+  Setpoint = 0;
+
+  calculate_IMU_error();
+  delay(20);
 }
 
+
+bool flag = false;
+double prvstartAngle = 0;
 void loop()
 {
-  /*
-  if (Serial.available() > 0)
-  {
-    mySerial.write(Serial.read());
-  }
-  if (mySerial.available() > 0)
-    Serial.write(mySerial.read());
-    */
-
-  if (mySerial.available() > 0)
+  
+   if (mySerial.available() > 0)
   {
     // parsing the json string
     parseJson(mySerial.read());
   }
+
+  if(prvstartAngle != startAngle){
+    Setpoint -= 1*radToDegree(startAngle);
+  }
+  prvstartAngle = startAngle;
+  
+  updateGyro();
+  Input = (double)angle;
+  myPID.Compute();
+
+  ML(Output);
+  MR(-Output);
+  
+}
+
+void ML(int val)
+{ // motor function left(val == speed value)
+
+  if (val > 0)
+  {
+
+    digitalWrite(ML_A2, HIGH);
+    digitalWrite(ML_A1, LOW);
+    analogWrite(EN_L, val);
+  }
+  else
+  {
+    digitalWrite(ML_A2, LOW);
+    digitalWrite(ML_A1, HIGH);
+    analogWrite(EN_L, abs(val));
+  }
+}
+
+void MR(int val)
+{ // motor function right(val == speed value)
+
+  if (val > 0)
+  {
+
+    digitalWrite(MR_A1, HIGH);
+    digitalWrite(MR_A2, LOW);
+    analogWrite(EN_R, val);
+  }
+  else
+  {
+    digitalWrite(MR_A1, LOW);
+    digitalWrite(MR_A2, HIGH);
+    analogWrite(EN_R, abs(val));
+  }
+}
+
+void movStraight(int spd)
+{
+  MR(spd);
+  ML(spd);
+}
+
+void updateGyro()
+{
+  previousTime = currentTime;                        // Previous time is stored before the actual time read
+  currentTime = millis();                            // Current time actual time read
+  elapsedTime = (currentTime - previousTime) / 1000; // Divide by 1000 to get seconds
+  Wire.beginTransmission(MPU);
+  Wire.write(0x43); // Gyro data first register address 0x43
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);                   // Read 4 registers total, each axis value is stored in 2 registers
+  GyroX = (Wire.read() << 8 | Wire.read()) / 32.75; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
+  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+
+  GyroX = GyroX - GyroErrorX; // GyroErrorX ~(-0.56)
+
+  angle = angle + GyroX * elapsedTime; // deg/s * s = deg
+  Serial.println(angle);
+}
+
+double radToDegree(double rads)
+{
+  return (float)(rads * 180 / PI);
 }
