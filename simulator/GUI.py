2409e448f3dfb2717482e7a68538ecc0db81f020
heshads <tharindudissa18@gmail.com>
2020-10-18 20:25:12 +0530

    updated functions

--- simulator/GUI.py
+++ simulator/GUI.py
@@ -1,34 +1,80 @@
 import cv2
 import numpy as np
+import random
+import math
+# postions and the angle data of robots
+# bots = [[120, 500, 10],
+#         [200, 600, 15],
+#         [400, 202, 60],
+#         [300, 100, 30],
+#         [700, 300, 80]]
 
-img = cv2.imread('/home/heshds/Academics/project/e16-3yp-obstacle-bots-for-swarm-robots/simulator/resources/grid2.jpg')
-logo = cv2.imread('/home/heshds/Academics/project/e16-3yp-obstacle-bots-for-swarm-robots/simulator/resources/logo.png',cv2.IMREAD_UNCHANGED)
-img = cv2.resize(img, (1000,1000))
-logo = cv2.resize(logo, (100,100))
 
-logo_alpha = logo[:,:,3]
+bots = list()
+BOT_COUNT = 10
+RADI = 50
+dS = 3
+background = cv2.imread('/home/heshds/Academics/project/e16-3yp-obstacle-bots-for-swarm-robots/simulator/resources/grid.png')
+bot_png = cv2.imread('/home/heshds/Academics/project/e16-3yp-obstacle-bots-for-swarm-robots/simulator/resources/logo.png',cv2.IMREAD_UNCHANGED)
 
-img_H, img_W, _ = img.shape
-png_H, png_W, _ = logo.shape
-x,y = 200,200
-f = 1
-r = 5
-t = 0
+
+background = cv2.resize(background, (1000, 1000))
+bot_png = cv2.resize(bot_png, (70,70))
+
+backg_H, backg_W, _ = background.shape
+bot_H, bot_W, _ = bot_png.shape
+
+print(backg_H, backg_W)
+print(bot_H, bot_W)
+
+
+def update(bots):
+    if len(bots) == 0:
+       for i in range(BOT_COUNT):
+           bots.append([random.randint(0, backg_H), random.randint(0, backg_H), random.randint(0, 360)])
+    else:
+        for bot in bots:
+            bot[0] = bot[0] + dS*np.cos(bot[2]*math.pi/180)
+            bot[1] = bot[1] + dS*np.sin(-bot[2]*math.pi/180)
+            bot[2] = bot[2] + 0.8
+                
+
+
+def rotate_image(image, angle):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+
+def draw_bots(bots):
+    overlay = np.zeros((backg_H, backg_W,4), dtype="uint8")
+    circles = np.zeros((backg_H, backg_W,3), dtype="uint8")
+    for bot in bots:
+        x = bot[0]
+        y = bot[1]
+        angle = bot[2]
+        x_start = int(x - bot_W/2)
+        y_start = int(y - bot_H/2)
+
+        x_start = 0 if (x_start<0) else ((backg_W - bot_W) if x_start>(backg_W - bot_W) else x_start)
+        y_start = 0 if (y_start<0) else ((backg_H - bot_H) if y_start>(backg_H - bot_H) else y_start)
+
+        bot_img = rotate_image(bot_png.copy(), angle)
+        overlay[ y_start:y_start+bot_W, x_start:x_start+bot_W] = bot_img
+        cv2.circle(overlay, (int(x_start+ bot_W/2), int(y_start+ bot_H/2)), RADI, (255,255,0,255),1)
+
+    return overlay
+        
 while True:
-    x = int(np.sin(2*3.17*f*t)*r )+x
-    y = int(np.cos(2*3.17*f*t)*r )+ y  
-    t = t+1
-    overlay = np.zeros((img_H, img_W,3),dtype="uint8")
-    overlay[x:x+png_H ,y:y+png_W] = logo[:,:,:3]
-    mask = np.zeros((img_H, img_W),dtype="uint8")
-    mask[x:x+png_H ,y:y+png_W] = logo_alpha
-    mask = cv2.bitwise_not(mask)
-
-    masked = cv2.bitwise_and(img,img,mask = mask) 
-    out = cv2.add(overlay,masked)
+    update(bots)
+    overlay = draw_bots(bots)
+    masked_backg = cv2.bitwise_and(background, background, mask = cv2.bitwise_not(overlay[:,:,3])) 
     
+    out = cv2.add(overlay[:,:,:3], masked_backg)
     cv2.imshow('test', out)
     
     key = cv2.waitKey(5)
     if key == 27:
         break
+
+
