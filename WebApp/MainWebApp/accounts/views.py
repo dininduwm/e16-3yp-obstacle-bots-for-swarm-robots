8fd666e13931583e400b026ebfcba308c7ac1da9
dtdinidu7 <dtdinidu7@gmail.com>
2020-11-07 12:43:28 +0530

    login procedure added

--- WebApp/MainWebApp/accounts/views.py
+++ WebApp/MainWebApp/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 # Create your views here.
 def signup_view(request):
@@ -13,6 +14,17 @@ def signup_view(request):
 
 def login_view(request):
     if (request.method == 'POST'):
-        print(request.POST['pass'])
+        user = User.objects.filter(username=request.POST['email'])
+        # checking for the ecistance of the user
+        if (len(user) == 0):
+            return HttpResponse('invalid user name')
+        else:
+            # checking for the password
+            if (user[0].check_password(request.POST['pass'])):
+                # logging in the user
+                login(request, user[0])
+                return HttpResponse('successfully loged in')
+            else:
+                return HttpResponse('invalid user name password combination')
         return HttpResponse('login')
     return render(request, 'accounts/login.html')
